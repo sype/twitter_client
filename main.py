a8742df25c151dab14d786851b5421ff94f7a75e
Sebastien Pincemail <extern.sebastien.pincemail@europcar.com>
2021-08-09 22:20:16 +0200

    initi

---
+++ main.py
@@ -0,0 +1,168 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+import os
+
+class TwitterClient(object):
+	'''
+	Generic Twitter Class for sentiment analysis.
+	'''
+	def __init__(self):
+		'''
+		Class constructor or initialization method.
+		'''
+		# keys and tokens from the Twitter Dev Console
+		consumer_key = os.environ['consumer_key'] 
+		consumer_secret = os.environ['consumer_secret']
+		access_token = os.environ['access_token'] 
+		access_token_secret = os.environ['access_token_secret']
+
+		# attempt authentication
+		try:
+			# create OAuthHandler object
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			# set access token and secret
+			self.auth.set_access_token(access_token, access_token_secret)
+			# create tweepy API object to fetch tweets
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
+
+	def clean_tweet(self, tweet):
+		'''
+		Utility function to clean tweet text by removing links, special characters
+		using simple regex statements.
+		'''
+
+
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
+
+	def get_tweet_sentiment(self, tweet):
+		'''
+		Utility function to classify sentiment of passed tweet
+		using textblob's sentiment method
+		'''
+		# create TextBlob object of passed tweet text
+		analysis = TextBlob(self.clean_tweet(tweet))
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			return 'neutral'
+		else:
+			return 'negative'
+
+	def get_sentiment(self, query, count = 10, result_type="mixed"):
+		'''
+		Main function to fetch tweets and parse them.
+		'''
+		# empty list to store parsed tweets
+		tweets = []
+
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search(q = query , count = count, result_type = result_type )
+
+			# parsing tweets one by one
+			for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+				parsed_tweet = {}
+
+				# saving text of tweet
+				parsed_tweet['text'] = tweet.text
+				# saving sentiment of tweet
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+				# appending parsed tweet to tweets list
+				if tweet.retweet_count > 0:
+					# if tweet has retweets, ensure that it is appended only once
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
+
+			# return parsed tweets
+			print(tweet.retweet_count)
+			return tweets
+
+		except tweepy.TweepError as e:
+			# print error (if any)
+			print("Error : " + str(e))
+
+	def get_trends_by_place(self, id,count,result_type):
+		'''
+			Function to get trends by location and return a liet of trendings hastag dans volume 
+		'''
+		
+		results = self.api.trends_place(id = id )
+		tweets = []
+		#print(results)
+		for location in results:
+			for trend in location["trends"]:
+				name = trend["name"]
+				tweet_volume = trend["tweet_volume"]
+				fetched_tweets = self.api.search(q = name , count = count, result_type = result_type )
+				for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+					parsed_tweet = {}
+
+					# saving text of tweet
+					parsed_tweet['text'] = tweet.text
+					# saving sentiment of tweet
+
+					# appending parsed tweet to tweets list
+					if tweet.retweet_count > 5000:
+					# if tweet has retweets, ensure that it is appended only once
+						if parsed_tweet not in tweets:
+							tweets.append(parsed_tweet)
+					else:
+						tweets.append(parsed_tweet)
+
+				# return parsed tweets
+				# TODO format the response with location associated to data
+				#print(name,tweet_volume )
+				return [tweets, [name,tweet_volume]] 
+
+
+def main():
+	# creating object of TwitterClient Class
+	api = TwitterClient()
+	# calling function to get tweets
+	tweets = api.get_sentiment(query = '#gitops', count = 400, result_type = "mixed")
+
+	# picking positive tweets from tweets
+	ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+	if len(ptweets) > 0:
+		# percentage of positive tweets
+		print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+	else:
+		msg = "List Empty"
+	# picking negative tweets from tweets
+	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+	if len(ntweets) or len(ptweets) > 0:
+		# percentage of negative tweets
+		print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+		# percentage of neutral tweets
+		print("Neutral tweets percentage: {} % ".format(100*(len(tweets) -(len( ntweets )+len( ptweets)))/len(tweets)))
+
+		# printing first 5 positive tweets
+		print("\n\nPositive tweets:")
+		for tweet in ptweets[:10]:
+			print(tweet['text'])
+
+		# printing first 5 negative tweets
+		print("\n\nNegative tweets:")
+		for tweet in ntweets[:10]:
+			print(tweet['text'])
+
+	else:
+		msg = "List Empty"
+		print(msg)
+
+	trend = api.get_trends_by_place(id=711080,count=400, result_type="mixed")	
+	print(trend)
+
+if __name__ == "__main__":
+	# calling main function
+	main()
